771fca82b2fbcfbccb29f60b8e3f7e218a993505
greendb16 <greendb16@gmail.com>
2019-06-12 14:58:25 -0400

    Final Commit

---
+++ src/EscapeSequenceII.java
@@ -0,0 +1,11 @@
+public class EscapeSequenceII {
+    public static void main(String[]args){
+
+        System.out.println( "Cost\tQuantity\tTotal\n" +
+                            "$1,000.00\t4\t$4,000.00\n" +
+                            "$50.00\t\t8\t$400.00\n"+
+                            "__________________________\n" +
+                            "Total:\t\t12\t$4,400.00");
+
+    }
+}
